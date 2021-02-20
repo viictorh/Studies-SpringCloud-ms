d84fc334c746b3a88576706db4b313a4be3da2da
Victor Bello <victor.bello@voxage.com.br>
2021-02-19 21:36:12 -0300

    Configuring eureka clients

--- hr-payroll/src/main/java/br/com/vhb/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/vhb/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,11 @@ package br.com.vhb.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
-@RibbonClient(name = "hr-worker")
 @SpringBootApplication
 public class HrPayrollApplication {
 
